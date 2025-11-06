dca85e8869b984a69031809cc77aad6161e357f9
Maricelahuaman <yucracuroroliber@gmail.com>
2025-11-06 09:22:18 -0500

    Update ClienteApiController.php

--- app/controllers/ClienteApiController.php
+++ app/controllers/ClienteApiController.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../models/Cliente.php';
 require_once __DIR__ . '/../models/Municipio.php';
 
-class ClienteApiControllers
+class ClienteApiController
 {
     private $clienteModel;
     private $municipioModel;
